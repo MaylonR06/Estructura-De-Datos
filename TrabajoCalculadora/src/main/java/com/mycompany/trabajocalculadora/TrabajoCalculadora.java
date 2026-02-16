b5e6480670a75bc68a6658dc868d1e9ecae1ab78
MaylonR06 <maylonriano1@gmail.com>
2026-02-16 11:10:23 -0500

    Add files via upload

---
+++ TrabajoCalculadora/src/main/java/com/mycompany/trabajocalculadora/TrabajoCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajocalculadora;
+
+/**
+ *
+ * @author maylo
+ */
+public class TrabajoCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
